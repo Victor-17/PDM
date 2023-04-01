1a18ad0df84bdb7e93d81d4308fe45c1e895cbc6
Victor Nobrega <vejapyassu@gmail.com>
2023-04-01 17:25:04 -0300

    subindo o jodo da fofoca

---
+++ JogodaFofoca/app/src/main/java/com/example/jogodafofoca/Cadastro.kt
@@ -0,0 +1,22 @@
+package com.example.jogodafofoca
+
+class Cadastro {
+    private var lista: MutableList<Fofoca>
+
+    init {
+        this.lista = mutableListOf()
+    }
+
+    fun add(fofoca: Fofoca) {
+        this.lista.add(fofoca)
+    }
+
+    fun size(): Int {
+        return this.lista.size
+    }
+
+    fun get(): Fofoca {
+        return this.lista.random()
+    }
+
+}
---
+++ JogodaFofoca/app/src/main/java/com/example/jogodafofoca/Fofoca.kt
@@ -0,0 +1,3 @@
+package com.example.jogodafofoca
+
+class Fofoca (var descricao: String, var status: Boolean): java.io.Serializable
